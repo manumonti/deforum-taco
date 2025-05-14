159e225064affa86714cc79724b3ed7b59f87c5c
Manuel Montenegro <manuel@nucypher.com>
2025-05-14 19:17:28 +0200

    Fix missing dependency useAccount from wagmi

--- app/(home)/(posts)/post/[slug]/page.tsx
+++ app/(home)/(posts)/post/[slug]/page.tsx
@@ -14,6 +14,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
+import { useAccount, useAccountEffect } from "wagmi";
 
 const CONTEXT_ID = env.NEXT_PUBLIC_CONTEXT_ID ?? "";
 const COMMENT_ID = env.NEXT_PUBLIC_COMMENT_ID ?? "";
